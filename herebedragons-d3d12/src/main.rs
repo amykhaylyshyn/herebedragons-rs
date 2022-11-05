dbaa82a3ce7a1e103b62946ac893bd5b35707788
Andriy Mykhaylyshyn <andriy.mykhaylyshyn@sigma.software>
2022-11-06 00:51:55 +0200

    Added resources, implementing scene loading

--- herebedragons-d3d12/src/main.rs
+++ herebedragons-d3d12/src/main.rs
@@ -3,14 +3,54 @@ mod gfx;
 mod hresult;
 mod renderer;
 
-use crate::error::Result;
+use anyhow::Result;
 use dotenv::dotenv;
-use gfx::backend_d3d12::BackendD3D12;
-use renderer::render_main;
+use serde::Deserialize;
+use std::collections::HashMap;
+use tokio::{
+    fs,
+    io::{self, AsyncReadExt},
+};
 
-fn main() -> Result<()> {
+#[derive(Debug, Deserialize)]
+struct Material {
+    pub textures: HashMap<String, String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Model {
+    pub mesh: String,
+    pub textures: HashMap<String, String>,
+}
+
+#[derive(Debug, Default, Deserialize)]
+struct Point3(f32, f32, f32);
+
+#[derive(Debug, Deserialize)]
+struct Object {
+    pub mesh: String,
+    pub material_name: String,
+    #[serde(default)]
+    pub offset: Point3,
+}
+
+#[derive(Debug, Deserialize)]
+struct Scene {
+    pub materials: HashMap<String, Material>,
+    pub objects: Vec<Object>,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
     dotenv().ok();
     env_logger::init();
 
-    render_main::<BackendD3D12>(800, 600, 3)
+    let mut f = fs::File::open("resources/scene.ron").await?;
+    let mut buf = Vec::new();
+    f.read_to_end(&mut buf).await?;
+
+    let scene: Scene = ron::from_str(std::str::from_utf8(&buf)?)?;
+    println!("{:?}", scene);
+
+    Ok(())
 }
