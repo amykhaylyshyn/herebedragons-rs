a42ddeceed699b5627cf3da29b6abd443cd279a9
Andriy Mykhaylyshyn <andriy.mykhaylyshyn@sigma.software>
2022-11-13 00:50:42 +0200

    Create window

--- herebedragons-d3d12/src/main.rs
+++ herebedragons-d3d12/src/main.rs
@@ -5,52 +5,37 @@ mod renderer;
 
 use anyhow::Result;
 use dotenv::dotenv;
-use serde::Deserialize;
-use std::collections::HashMap;
-use tokio::{
-    fs,
-    io::{self, AsyncReadExt},
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
 };
 
-#[derive(Debug, Deserialize)]
-struct Material {
-    pub textures: HashMap<String, String>,
-}
-
-#[derive(Debug, Deserialize)]
-struct Model {
-    pub mesh: String,
-    pub textures: HashMap<String, String>,
-}
-
-#[derive(Debug, Default, Deserialize)]
-struct Point3(f32, f32, f32);
-
-#[derive(Debug, Deserialize)]
-struct Object {
-    pub mesh: String,
-    pub material_name: String,
-    #[serde(default)]
-    pub offset: Point3,
-}
-
-#[derive(Debug, Deserialize)]
-struct Scene {
-    pub materials: HashMap<String, Material>,
-    pub objects: Vec<Object>,
-}
-
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
     dotenv().ok();
     env_logger::init();
 
-    let mut f = fs::File::open("resources/scene.ron").await?;
-    let mut buf = Vec::new();
-    f.read_to_end(&mut buf).await?;
+    let window_width = 1280f64;
+    let window_height = 720f64;
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_inner_size(winit::dpi::Size::Logical(winit::dpi::LogicalSize::new(
+            window_width,
+            window_height,
+        )))
+        .with_title("D3D12 Window")
+        .build(&event_loop)
+        .unwrap();
 
-    let scene: Scene = ron::from_str(std::str::from_utf8(&buf)?)?;
-    println!("{:?}", scene);
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
 
-    Ok(())
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => control_flow.set_exit(),
+            _ => (),
+        }
+    });
 }
